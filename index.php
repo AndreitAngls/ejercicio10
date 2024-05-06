4916a1358aa042bd07b42e8cbb3179b819339d80
Andreita <rcalderonm2@fcpn.edu.bo>
2024-05-06 03:20:16 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title> EJERCICIO-10 </title>
+	</head>
+	
+	<body style="background-image: url(fondo.jpg); background-position: center; background-repeat: no-repeat; background-size: cover; background-attachment: fixed;">
+
+		<h1> 
+			<font color="white" style="font-family:courier,arial,helvética"> 
+				<?php 
+					echo "HOLA MUNDO!!! MI NOMBRE ES";
+				?>
+			</font>
+		</h1>
+
+		<h1> 
+			<font color="Crimson" style="font-family:Lucida Handwriting"> 
+				<?php 
+					echo "<center> ROCIO ANDREA CALDERON MARQUEZ </center>"; 
+				?>
+				 <br>
+			</font>
+			<font color="orange" style="font-family:Copperplate"> 
+				<?php 
+					echo "<center> SOY DE LA MATERIA INF-324 -> 'PROGRAMACION MULTIMEDIA' </center>"; 
+				?>
+				 <br>
+			</font>
+		</h1>
+
+	</body>
+</html>
